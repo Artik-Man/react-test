df994f4275a21035588ab58a8a435d7661eab773
Artik <mail@artik.me>
2019-01-23 16:57:37 +0300

    first commit

---
+++ src/components/app/component.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Template from "./template";
+
+class App extends Component {
+  render() {
+    return <Template />;
+  }
+}
+
+export default App;
